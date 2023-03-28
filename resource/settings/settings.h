e26f1840a388f7e902d5c8ca9312e4aa08171f7b
elchedli <shidonosan@gmail.com>
2023-03-28 18:11:37 +0100

    personal parts in the game

--- resource/settings/settings.h
+++
@@ -1,7 +0,0 @@
-#ifndef SETTINGS_H_INCLUDE
-#define SETTINGS_H_INCLUDE
-
-int InsideOne(int* slctd);
-void settingsMenu(SDL_Surface* screen, Background menu, Background menutop, Button* button, Mix_Chunk* SoundButtonHover, Mix_Chunk* SoundButtonClick, Background settingsBackground, Background circle, int inGame, keybinds *kb, int j1, int j2, Text txt);
-
-#endif //MENU_H_INCLUDE
